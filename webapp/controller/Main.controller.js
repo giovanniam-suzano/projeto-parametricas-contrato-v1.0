7df3a72105f3e96d2d97c6f60b7059ac80fc60e5
GIOVANNI ALVES MEDICI <giovanniam>
2026-04-15 19:03:40 +0000

    DeMudanca de scripts para insecao de variaveis na tabela pagina principal e no dialog puxar e mostrar o dados selecionado. PS: Somente 1 dado por vez

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -7,8 +7,31 @@ sap.ui.define([
 
     return Controller.extend("com.parametricas.parametricasapp.controller.Main", {
         onInit: function () {
-            // Inicializações se necessárias (Models, Routings)
-        },
+    var oModel = new sap.ui.model.json.JSONModel({
+        contratos: [
+            {
+                id: "1",
+                material: "Parafuso",
+                quantidade: 12,
+                unidade: 10,
+                valorUnitario: "15,00",
+                valorTotal: "1.800,00 REAL",
+                valorSugerido: "1.875,00 REAL"
+            },
+            {
+                id: "2",
+                material: "Martelo",
+                quantidade: 5,
+                unidade: 1,
+                valorUnitario: "247,00",
+                valorTotal: "1.235,00 REAL",
+                valorSugerido: "1.345,00 REAL"
+            }
+        ]
+    });
+
+    this.getView().setModel(oModel);
+},
 
         /* =========================================================== */
         /* Ações da Tabela / Header                                    */
@@ -21,8 +44,23 @@ sap.ui.define([
         },
 
         onEditarPress: function () {
-            MessageToast.show("Indo para Edição !!");
-            this.onAbrirParametricas();
+            var oTable = this.byId("tabelaContratos");
+            var aItems = oTable.getSelectedItems();
+
+            if (aItems.length === 0) {
+                MessageToast.show("Selecione um item para editar");
+                return;
+            }
+
+            if (aItems.length > 1) {
+                MessageToast.show("Selecione apenas um item para editar");
+                return;
+            }
+
+            var oSelectedItem = aItems[0];
+            var oContext = oSelectedItem.getBindingContext();
+
+            this.onAbrirParametricas(oContext);
         },
 
         onConcluidoPress: function () {
@@ -33,14 +71,13 @@ sap.ui.define([
         /* Gerenciamento do Fragment / Modal de Paramétricas           */
         /* =========================================================== */
 
-        onAbrirParametricas: function () {
+        onAbrirParametricas: function (oContext) {
             var oView = this.getView();
 
-            // Padrão SAPUI5 para lazy loading de Fragments
             if (!this._pParametricasDialog) {
                 this._pParametricasDialog = Fragment.load({
                     id: oView.getId(),
-                    name: "com.parametricas.parametricasapp.view.ParametricasDialog", // Ajuste para o seu namespace
+                    name: "com.parametricas.parametricasapp.view.ParametricasDialog",
                     controller: this
                 }).then(function (oDialog) {
                     oView.addDependent(oDialog);
@@ -49,6 +86,7 @@ sap.ui.define([
             }
 
             this._pParametricasDialog.then(function (oDialog) {
+                oDialog.setBindingContext(oContext); 
                 oDialog.open();
             });
         },
